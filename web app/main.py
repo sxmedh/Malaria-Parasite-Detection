706ee6ccb14edd4023f11a7c3e0097ef6e29ff4f
Sumedh Gole <82237532+sxmedh@users.noreply.github.com>
2024-05-04 17:18:15 +0530

    web app final

--- web app/main.py
+++ web app/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, flash
 from werkzeug.utils import secure_filename
 import os
+import cv2
 from predict_utils import predict
 
 UPLOAD_FOLDER = 'uploads'
@@ -11,27 +12,31 @@ app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def processImage(filename):
-    if(predict(filename)):
-        return "infected"
-    return "uninfected"
+    if (predict(filename)):
+        return "Infected"
+    return "Uninfected"
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
-    if request.method == "POST": 
+    if request.method == "POST":
         if 'file' not in request.files:
             flash('No file part')
             return "error"
@@ -43,8 +48,9 @@ def edit():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             result = processImage(filename)
-            flash(f'<img src="uploads/{filename}.png" class="img-fluid" />')
-            return render_template("result.html",filename=filename,result=result)
+            img = cv2.imread(f"uploads/{filename}")
+            cv2.imwrite(f"static/{filename}", img)
+            return render_template("result.html", filename=filename, result=result)
 
     return render_template("index.html")
 
